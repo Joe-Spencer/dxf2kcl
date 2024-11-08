b4696c12e3ee02e0d979f26ecdf6fabfdf0a95fa
Joe <31321326+Joe-Spencer@users.noreply.github.com>
2024-11-08 14:05:43 -0500

    Added more comments to the file

--- dxf2kcl.py
+++ dxf2kcl.py
@@ -15,20 +15,22 @@ def convert_dxf_to_kcl(dxf_path, kcl_path):
             num_entities += 1
             kcl_file.write(f"sketch" + str(num_entities)  + f"= startSketchOn('XY')" + "\n")
             try:
+                #account for lines 
                 if entity.dxftype() == "LINE":
                     x1=entity.dxf.start[0]
                     y1=entity.dxf.start[1]
                     kcl_file.write(f"   |> startProfileAt([{x1}, {y1}], %)\n")
-                    
-
-                    x2=entity.dxf.end[0]-entity.dxf.start[0]
-                    y2=entity.dxf.end[1]-entity.dxf.start[1]
-                    kcl_file.write(f"   |> line([{x2}, {y2}], %)\n")
+                    #calulate the delta x and delta y
+                    deltaX=entity.dxf.end[0]-entity.dxf.start[0]
+                    deltaY=entity.dxf.end[1]-entity.dxf.start[1]
+                    kcl_file.write(f"   |> line([{deltaX}, {deltaY}], %)\n")
+                #account for circles   
                 elif entity.dxftype() == "CIRCLE":
                     x=entity.dxf.center[0]
                     y=entity.dxf.center[1]
                     radius = entity.dxf.radius
                     kcl_file.write("   |> circle({"+"center: [" + str(x) + ","+str(y)+"], radius: "+str(radius)+"}, %)\n")
+                #account for arcs
                 elif entity.dxftype() == "ARC":
                     x1=entity.start_point[0]
                     y1=entity.start_point[1]
@@ -37,6 +39,7 @@ def convert_dxf_to_kcl(dxf_path, kcl_path):
                     angleend = entity.dxf.end_angle
                     radius = entity.dxf.radius
                     kcl_file.write("     |> arc({"+"angleStart: " + str(anglestart) + ", angleEnd:"+str(angleend)+", radius: "+str(radius)+"}, %)\n")
+                #account for most types of polylines
                 elif entity.dxftype() == "LWPOLYLINE":
                     points = entity.get_points()
                     start = points[0]
@@ -44,6 +47,7 @@ def convert_dxf_to_kcl(dxf_path, kcl_path):
                     for point in points[1:]:
                         kcl_file.write(f"     |> lineTo([{point[0]}, {point[1]}], %)\n")
                     kcl_file.write("     |> close(%)\n")
+                #splines are not currently supported
                 elif entity.dxftype() == "SPLINE":
                     print("sorry no spline for you")
                     kcl_file.write("// sorry no spline for you\n")
